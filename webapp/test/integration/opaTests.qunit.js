f8b91338688a8642e3897dd9bd7961053c038a23
I546033 <ovidiu-adrian.hulea@sap.com>
2022-11-30 17:48:13 +0000

    “first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["tpl/deliveries/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"tpl/deliveries/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
